1d6ce4c13166404b164b5fd230e8ea4cda9deccd
Oskar O <oskar.orzelowski@netguru.co>
2019-01-07 15:56:47 +0100

    Implement add Venue page.

--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ const { languagesPattern } = require("./consts");
 module.exports = routes()
   .add("landing", `/:lng(${languagesPattern})/`)
   .add("register", `/:lng(${languagesPattern})/register`)
-  .add("add/manager", `/:lng(${languagesPattern})/add/manager`);
+  .add("add/manager", `/:lng(${languagesPattern})/add/manager`)
+  .add("add/venue", `/:lng(${languagesPattern})/add/venue`);
