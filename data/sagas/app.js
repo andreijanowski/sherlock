6ca1d9a0c123fff437ef9845f07f661d767a15bf
Marcin Kadlubowski <marcinkadlubowski@pqstudio.pl>
2019-02-06 15:32:22 +0100

    fix refresh data

--- data/sagas/app.js
+++ data/sagas/app.js
@@ -1,9 +1,30 @@
-import { all, put, takeEvery } from "redux-saga/effects";
+import { all, put, takeEvery, select } from "redux-saga/effects";
 import { startSaga as startSagaAction } from "actions/app";
-import { APP_START } from "types/app";
+import { APP_START, PATH_CHANGED } from "types/app";
+import { fetchProfileBusiness } from "actions/users";
 
 function* startSaga() {
   yield put(startSagaAction());
 }
 
-export default all([takeEvery(APP_START, startSaga)]);
+function* handlePatchChangeSaga({ payload: { path } }) {
+  switch (path) {
+    case "/app/profile/basicInformation":
+    case "/app/profile/contactInformation":
+    case "/app/profile/additionalInformation":
+    case "/app/profile/openingHours":
+    case "/app/profile/picturesAndMenus": {
+      const id = yield select(state => state.users.currentBusiness.data.id);
+      yield put(fetchProfileBusiness(id));
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
+
+export default all([
+  takeEvery(APP_START, startSaga),
+  takeEvery(PATH_CHANGED, handlePatchChangeSaga)
+]);
