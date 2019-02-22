f7f424d14f0a05decea24e47e55953c6e4aca209
Marcin Kadlubowski <marcinkadlubowski@pqstudio.pl>
2019-02-22 21:48:46 +0100

    update styles of dnd elements

--- sections/lefood/orders/styled.jsx
+++ sections/lefood/orders/styled.jsx
@@ -1,20 +1,41 @@
 import styled from "styled-components";
 import { Flex, Box } from "@rebass/grid";
 
-export const ColumnsWrapper = styled(Flex).attrs({ mx: -2 })``;
+export const ColumnsWrapper = styled(Flex).attrs({ mx: -2 })`
+  overflow: auto;
+`;
 export const ColumnWrapper = styled(Flex).attrs({
   flexDirection: "column",
   width: 1 / 5,
   p: 2
+})`
+  min-width: 280px;
+`;
+export const ColumnHeader = styled(Flex).attrs({
+  py: 3,
+  px: 2,
+  justifyContent: "space-between",
+  alignItems: "center"
 })``;
-export const ColumnHeader = styled(Flex).attrs({})``;
-export const ColumnTitle = styled(Box).attrs({})``;
-export const OrdersNumber = styled(Box).attrs({})``;
+export const ColumnTitle = styled(Box).attrs({})`
+  font-size: ${p => p.theme.fontSizes.f21};
+  color: rgb(${p => p.theme.colors.dark});
+`;
+export const OrdersNumber = styled(Box).attrs({ width: 32 })`
+  height: 32px;
+  line-height: 32px;
+  text-align: center;
+  border-radius: 16px;
+  background-color: rgba(${p => p.theme.colors.blue}, 0.1);
+  color: rgb(${p => p.theme.colors.blue});
+  font-size: ${p => p.theme.fontSizes.f14};
+  font-weight: ${p => p.theme.fontWeights.bold};
+`;
 export const OrdersWrapper = styled(Flex).attrs({
   flexDirection: "column",
   p: 2
 })`
-  background-color: rgb(${p => p.theme.colors.snuff});
+  background-color: rgb(${p => p.theme.colors.linkWater});
   border-radius: ${p => p.theme.radius.default};
   height: 100%;
 `;
@@ -22,7 +43,7 @@ export const OrderWrapper = styled(Flex).attrs({
   flexDirection: "column",
   mb: 2
 })`
-  border: 1px solid rgb(${p => p.theme.colors.lavenderGray});
+  border: 1px solid rgb(${p => p.theme.colors.linkWaterDark});
   border-radius: ${p => p.theme.radius.default};
   overflow: hidden;
 `;
@@ -33,8 +54,8 @@ export const OrderHeader = styled(Flex).attrs({
   justifyContent: "space-between"
 })`
   line-height: 24px;
-  background-color: rgb(${p => p.theme.colors.background});
-  border-bottom: 1px solid rgb(${p => p.theme.colors.lavenderGray});
+  background-color: rgb(${p => p.theme.colors.titanWhite});
+  border-bottom: 1px solid rgb(${p => p.theme.colors.linkWaterDark});
 `;
 export const OrderPrice = styled(Box).attrs({})`
   color: rgb(${p => p.theme.colors.dark});
@@ -42,7 +63,7 @@ export const OrderPrice = styled(Box).attrs({})`
   font-size: ${p => p.theme.fontSizes.f14};
 `;
 export const OrderTime = styled(Box).attrs({})`
-  color: rgba(${p => p.theme.colors.abbey});
+  color: rgba(${p => p.theme.colors.rollingStone});
   font-size: ${p => p.theme.fontSizes.f12};
 `;
 export const OrderDetails = styled(Flex).attrs({
@@ -52,6 +73,7 @@ export const OrderDetails = styled(Flex).attrs({
   background-color: rgb(${p => p.theme.colors.white});
 `;
 export const OrderDetail = styled(Box).attrs({})`
-  color: rgba(${p => p.theme.colors.abbey});
+  color: rgba(${p => p.theme.colors.rollingStone});
   font-size: ${p => p.theme.fontSizes.f14};
+  line-height: 24px;
 `;
