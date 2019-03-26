cdaebd8c0a6bf309408e560f52972447072d5b53
Przemysław Gwoździej <przemyslaw.gwozdziej@netguru.co>
2019-03-26 15:45:19 +0100

    Fix routes paths - remove "/" at end of the path

--- routes.js
+++ routes.js
@@ -2,58 +2,58 @@ const routes = require("next-routes");
 const { languagesPattern } = require("./consts");
 
 module.exports = routes()
-  .add("landing", `/:lng(${languagesPattern})/`)
-  .add("login", `/:lng(${languagesPattern})/login/`)
-  .add("resetPassword", `/:lng(${languagesPattern})/reset-password/`)
-  .add("register", `/:lng(${languagesPattern})/register/`)
-  .add("add/manager", `/:lng(${languagesPattern})/add/manager/`)
-  .add("add/venue", `/:lng(${languagesPattern})/add/venue/`)
-  .add("app/index", `/:lng(${languagesPattern})/app/`)
+  .add("landing", `/:lng(${languagesPattern})`)
+  .add("login", `/:lng(${languagesPattern})/login`)
+  .add("resetPassword", `/:lng(${languagesPattern})/reset-password`)
+  .add("register", `/:lng(${languagesPattern})/register`)
+  .add("add/manager", `/:lng(${languagesPattern})/add/manager`)
+  .add("add/venue", `/:lng(${languagesPattern})/add/venue`)
+  .add("app/index", `/:lng(${languagesPattern})/app`)
   .add(
     "app/profile/basicInformation",
-    `/:lng(${languagesPattern})/app/profile/basic-information/`
+    `/:lng(${languagesPattern})/app/profile/basic-information`
   )
-  .add("app/profile/members", `/:lng(${languagesPattern})/app/profile/members/`)
+  .add("app/profile/members", `/:lng(${languagesPattern})/app/profile/members`)
   .add(
     "app/profile/contactInformation",
-    `/:lng(${languagesPattern})/app/profile/contact-information/`
+    `/:lng(${languagesPattern})/app/profile/contact-information`
   )
   .add(
     "app/profile/additionalInformation",
-    `/:lng(${languagesPattern})/app/profile/additional-information/`
+    `/:lng(${languagesPattern})/app/profile/additional-information`
   )
   .add(
     "app/profile/openingHours",
-    `/:lng(${languagesPattern})/app/profile/opening-hours/`
+    `/:lng(${languagesPattern})/app/profile/opening-hours`
   )
   .add(
     "app/profile/picturesAndMenus",
-    `/:lng(${languagesPattern})/app/profile/pictures-and-menus/`
+    `/:lng(${languagesPattern})/app/profile/pictures-and-menus`
   )
   .add(
     "app/settings/basicInformation",
-    `/:lng(${languagesPattern})/app/settings/basic-information/`
+    `/:lng(${languagesPattern})/app/settings/basic-information`
   )
   .add(
     "app/settings/billing",
-    `/:lng(${languagesPattern})/app/settings/billing/`
+    `/:lng(${languagesPattern})/app/settings/billing`
   )
   .add(
     "app/settings/password",
-    `/:lng(${languagesPattern})/app/settings/password/`
+    `/:lng(${languagesPattern})/app/settings/password`
   )
-  .add("app/catering/day", `/:lng(${languagesPattern})/app/catering/day/`)
-  .add("app/catering/week", `/:lng(${languagesPattern})/app/catering/week/`)
-  .add("app/catering/month", `/:lng(${languagesPattern})/app/catering/month/`)
-  .add("app/catering/year", `/:lng(${languagesPattern})/app/catering/year/`)
-  .add("app/catering/create", `/:lng(${languagesPattern})/app/catering/create/`)
-  .add("app/lefood/orders", `/:lng(${languagesPattern})/app/lefood/orders/`)
+  .add("app/catering/day", `/:lng(${languagesPattern})/app/catering/day`)
+  .add("app/catering/week", `/:lng(${languagesPattern})/app/catering/week`)
+  .add("app/catering/month", `/:lng(${languagesPattern})/app/catering/month`)
+  .add("app/catering/year", `/:lng(${languagesPattern})/app/catering/year`)
+  .add("app/catering/create", `/:lng(${languagesPattern})/app/catering/create`)
+  .add("app/lefood/orders", `/:lng(${languagesPattern})/app/lefood/orders`)
   .add(
     "app/lefood/deliveries",
-    `/:lng(${languagesPattern})/app/lefood/delivery-area/`
+    `/:lng(${languagesPattern})/app/lefood/delivery-area`
   )
-  .add("app/lefood/menu", `/:lng(${languagesPattern})/app/lefood/menu/`)
+  .add("app/lefood/menu", `/:lng(${languagesPattern})/app/lefood/menu`)
   .add(
     "app/lefood/orderingHours",
-    `/:lng(${languagesPattern})/app/lefood/ordering-hours/`
+    `/:lng(${languagesPattern})/app/lefood/ordering-hours`
   );
