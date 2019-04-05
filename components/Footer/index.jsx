c3d508ec14c00df18e4c9369248baeec18905106
Przemysław Gwoździej <przemyslaw.gwozdziej@netguru.co>
2019-04-05 12:34:32 +0200

    Fetch current year in footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -15,42 +15,44 @@ import NavigationList from "./navigationList";
 
 const namespaces = ["footer"];
 
-const Footer = ({ t }) => (
-  <FooterWrapper>
-    <TextWrapper>
-      <FoodetectiveLogoWrapper>
-        <FoodetectiveLogo />
-      </FoodetectiveLogoWrapper>
-      <MenuWrapper>
-        <Box width={[1 / 2, "auto"]} p={2}>
-          <ColumnTitle>{t("navigation.columnOne.title")}</ColumnTitle>
-          <NavigationList {...{ t, columnName: "columnOne" }} />
-        </Box>
-        <Box width={[1 / 2, "auto"]} p={2}>
-          <ColumnTitle>{t("navigation.columnTwo.title")}</ColumnTitle>
-          <NavigationList {...{ t, columnName: "columnTwo" }} />
-        </Box>
-        <Box width={[1 / 2, "auto"]} p={2}>
-          <ColumnTitle>{t("navigation.columnThree.title")}</ColumnTitle>
-          <NavigationList {...{ t, columnName: "columnThree" }} />
-        </Box>
-        <Box width={[1 / 2, "auto"]} p={2}>
-          <ColumnTitle>{t("navigation.columnFour.title")}</ColumnTitle>
-          <NavigationList
-            {...{
-              t,
-              columnName: "columnFour",
-              hrefs: [
-                "https://www.facebook.com/sherlock.foodetective/",
-                "https://www.linkedin.com/company/13981713/",
-                "https://twitter.com/GetSherlockNow",
-                "https://www.instagram.com/sherlock.foodetective.co"
-              ]
-            }}
-          />
-        </Box>
-        {/* hide until we don't have mobile app */}
-        {/* <Box>
+const Footer = ({ t }) => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <FooterWrapper>
+      <TextWrapper>
+        <FoodetectiveLogoWrapper>
+          <FoodetectiveLogo />
+        </FoodetectiveLogoWrapper>
+        <MenuWrapper>
+          <Box width={[1 / 2, "auto"]} p={2}>
+            <ColumnTitle>{t("navigation.columnOne.title")}</ColumnTitle>
+            <NavigationList {...{ t, columnName: "columnOne" }} />
+          </Box>
+          <Box width={[1 / 2, "auto"]} p={2}>
+            <ColumnTitle>{t("navigation.columnTwo.title")}</ColumnTitle>
+            <NavigationList {...{ t, columnName: "columnTwo" }} />
+          </Box>
+          <Box width={[1 / 2, "auto"]} p={2}>
+            <ColumnTitle>{t("navigation.columnThree.title")}</ColumnTitle>
+            <NavigationList {...{ t, columnName: "columnThree" }} />
+          </Box>
+          <Box width={[1 / 2, "auto"]} p={2}>
+            <ColumnTitle>{t("navigation.columnFour.title")}</ColumnTitle>
+            <NavigationList
+              {...{
+                t,
+                columnName: "columnFour",
+                hrefs: [
+                  "https://www.facebook.com/sherlock.foodetective/",
+                  "https://www.linkedin.com/company/13981713/",
+                  "https://twitter.com/GetSherlockNow",
+                  "https://www.instagram.com/sherlock.foodetective.co"
+                ]
+              }}
+            />
+          </Box>
+          {/* hide until we don't have mobile app */}
+          {/* <Box>
           <ColumnTitle>{t("navigation.columnFive.title")}</ColumnTitle>
           <nav>
             <ListWrapper>
@@ -67,14 +69,15 @@ const Footer = ({ t }) => (
             </ListWrapper>
           </nav>
         </Box> */}
-      </MenuWrapper>
-    </TextWrapper>
-    <CopyrightWrapper>
-      <CopyrightNote>{t("copyrightNote")}</CopyrightNote>
-      <LanguageSwitcher withBorder={false} listPosition="top" />
-    </CopyrightWrapper>
-  </FooterWrapper>
-);
+        </MenuWrapper>
+      </TextWrapper>
+      <CopyrightWrapper>
+        <CopyrightNote>{t("copyrightNote", { currentYear })}</CopyrightNote>
+        <LanguageSwitcher withBorder={false} listPosition="top" />
+      </CopyrightWrapper>
+    </FooterWrapper>
+  );
+};
 
 Footer.propTypes = {
   t: func.isRequired
