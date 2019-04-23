811f6375dfc9820460df8fddf2b4d469b5a1ddd4
Przemysław Gwoździej <przemyslaw.gwozdziej@netguru.co>
2019-04-23 12:09:19 +0200

    Keep user login - synchronize token between tabs from local storage - refresh token when it expires on the current session

--- data/sagas/auth.js
+++ data/sagas/auth.js
@@ -1,4 +1,14 @@
-import { takeEvery, put, all, select, takeLatest } from "redux-saga/effects";
+import {
+  takeEvery,
+  put,
+  all,
+  select,
+  takeLatest,
+  call,
+  take,
+  fork
+} from "redux-saga/effects";
+import { eventChannel } from "redux-saga";
 import { REHYDRATE } from "redux-persist";
 import {
   fetchProfile,
@@ -27,10 +37,48 @@ import {
   CHANGE_PASSWORD_BY_TOKEN_SUCCESS
 } from "types/auth";
 import Notifications from "react-notification-system-redux";
-import { refreshToken as refresh } from "actions/auth";
+import {
+  refreshToken as refresh,
+  setAuthSynchronizedFromStorage
+} from "actions/auth";
 import { setCurrentBusiness } from "actions/app";
 
-function* initialTokenRefresh() {
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function createLocalstorageChannel() {
+  return eventChannel(emit => {
+    const watchHandler = event => {
+      emit(event);
+    };
+    window.addEventListener("storage", watchHandler);
+    const unsubscribe = () => {
+      window.removeEventListener("storage", watchHandler);
+    };
+
+    return unsubscribe;
+  });
+}
+
+function* syncWithLocalStorage() {
+  const storageChanel = yield call(createLocalstorageChannel);
+  while (true) {
+    const payload = yield take(storageChanel);
+    if (payload.key === "persist:sherlock") {
+      const parsedNewValue = JSON.parse(payload.newValue);
+      const parsedOldValue = JSON.parse(payload.oldValue);
+      if (parsedNewValue.auth !== parsedOldValue.auth) {
+        const parsedAuth = JSON.parse(parsedNewValue.auth);
+        yield put(setAuthSynchronizedFromStorage(parsedAuth));
+      }
+    }
+  }
+}
+
+function* subscribeForRefresh() {
+  const { expiresIn } = yield select(state => state.auth);
+  // 5 minutes before expires
+  const msBeforeExpires = 300000;
+  yield delay(expiresIn * 1000 - msBeforeExpires);
   const refreshToken = yield select(state => state.auth.refreshToken);
   if (refreshToken) {
     yield put(refresh({ refreshToken }));
@@ -69,6 +117,21 @@ function* fetchUserData() {
   }
 }
 
+function* initialTokenRefresh() {
+  const refreshToken = yield select(state => state.auth.refreshToken);
+  const createdAt = yield select(state => state.auth.createdAt);
+  const expiresIn = yield select(state => state.auth.expiresIn);
+  if (refreshToken) {
+    if ((createdAt + expiresIn) * 1000 < new Date().getTime()) {
+      yield put(refresh({ refreshToken }));
+    } else {
+      yield fetchUserData();
+      yield fork(syncWithLocalStorage);
+      yield subscribeForRefresh();
+    }
+  }
+}
+
 function* showSuccessPasswordChangeMsg() {
   yield put(
     Notifications.success({
@@ -103,6 +166,24 @@ export default all([
       REFRESH_TOKEN_SUCCESS
     ],
     fetchUserData
+  ),
+  takeEvery(
+    [
+      LOGIN_SUCCESS,
+      REGISTER_SUCCESS,
+      FACEBOOK_LOGIN_SUCCESS,
+      REFRESH_TOKEN_SUCCESS
+    ],
+    syncWithLocalStorage
+  ),
+  takeEvery(
+    [
+      LOGIN_SUCCESS,
+      REGISTER_SUCCESS,
+      FACEBOOK_LOGIN_SUCCESS,
+      REFRESH_TOKEN_SUCCESS
+    ],
+    subscribeForRefresh
   ),
   takeEvery(CHANGE_PASSWORD_SUCCESS, showSuccessPasswordChangeMsg),
   takeEvery(RESET_PASSWORD_SUCCESS, showSuccessResetPasswordMsg),
