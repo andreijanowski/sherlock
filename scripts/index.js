cf1fef90740f68a5033018a05b0504c5459b7f68
Marcin Kadlubowski <marcinkadlubowski@pqstudio.pl>
2019-04-26 12:53:29 +0200

    clean

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 export { default as FacebookPixelScript } from "./facebookPixel";
-export { default as GoogleAnalyticsScript } from "./googleAnalytics";
 export {
   default as GoogleTagManagerScript,
   noscript as GoogleTagManagerNoscript
