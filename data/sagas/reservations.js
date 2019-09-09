903321b50d19fe1ef220bb4efd70a92b3bc6f41f
Marcin Kadlubowski <marcinkadlubowski@pqstudio.pl>
2019-09-09 15:02:04 +0200

    [TFD-1948] Users cannot change the table attributed to a booking

--- data/sagas/reservations.js
+++ data/sagas/reservations.js
@@ -1,6 +1,10 @@
 /* eslint-disable camelcase */
 import { takeEvery, put, all } from "redux-saga/effects";
-import { PATCH_RESERVATION_SUCCESS } from "types/reservations";
+import {
+  PATCH_RESERVATION_SUCCESS,
+  PATCH_RESERVATION_REJECT_SUCCESS,
+  PATCH_RESERVATION_CANCEL_SUCCESS
+} from "types/reservations";
 import { fetchReservation } from "actions/reservations";
 
 function* getReservation({
@@ -13,4 +17,13 @@ function* getReservation({
   yield put(fetchReservation(id));
 }
 
-export default all([takeEvery([PATCH_RESERVATION_SUCCESS], getReservation)]);
+export default all([
+  takeEvery(
+    [
+      PATCH_RESERVATION_SUCCESS,
+      PATCH_RESERVATION_REJECT_SUCCESS,
+      PATCH_RESERVATION_CANCEL_SUCCESS
+    ],
+    getReservation
+  )
+]);
