5dfbfe9ffd3c9a8b415f7d370788f7e5e82869c5
Marcin Kadlubowski <marcinkadlubowski@pqstudio.pl>
2019-09-18 12:09:12 +0200

    [TFD-2000] Handle pusher for bookings

--- data/sagas/reservations.js
+++ data/sagas/reservations.js
@@ -1,9 +1,10 @@
 /* eslint-disable camelcase */
-import { takeEvery, put, all } from "redux-saga/effects";
+import { takeEvery, put, all, select } from "redux-saga/effects";
 import {
   PATCH_RESERVATION_SUCCESS,
   PATCH_RESERVATION_REJECT_SUCCESS,
-  PATCH_RESERVATION_CANCEL_SUCCESS
+  PATCH_RESERVATION_CANCEL_SUCCESS,
+  HANDLE_RESERVATION_UPDATE
 } from "types/reservations";
 import { fetchReservation } from "actions/reservations";
 
@@ -17,6 +18,19 @@ function* getReservation({
   yield put(fetchReservation(id));
 }
 
+function* updateReservation({ payload: { business_uuid, reservation_uuid } }) {
+  const id = yield select(state =>
+    state
+      .getIn(["users", "currentBusiness", "data", "businesses"])
+      .first()
+      .get("id")
+  );
+
+  if (business_uuid === id) {
+    yield put(fetchReservation(reservation_uuid));
+  }
+}
+
 export default all([
   takeEvery(
     [
@@ -25,5 +39,6 @@ export default all([
       PATCH_RESERVATION_CANCEL_SUCCESS
     ],
     getReservation
-  )
+  ),
+  takeEvery([HANDLE_RESERVATION_UPDATE], updateReservation)
 ]);
