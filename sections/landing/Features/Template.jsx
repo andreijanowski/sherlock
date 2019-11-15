cffe407cb37bdab99b92743d68de4b024a9c92ab
Przemysław Gwoździej <przemyslaw.gwozdziej@netguru.co>
2019-11-15 13:45:01 +0100

    improve the quality of displayed features screens

--- sections/landing/Features/Template.jsx
+++ sections/landing/Features/Template.jsx
@@ -6,15 +6,20 @@ import { Screen } from "./Template.styled";
 
 const Template = ({ t, activeNavItem }) => (
   <Flex flexWrap="wrap" m={-2}>
-    <Box width={[1, 1 / 2]} p={2}>
+    <Box flex={1} width={[1, 1 / 2]} p={2}>
       <H2 white>{t(`features.subsections.${activeNavItem}.header`)}</H2>
       <Paragraph white>
         {t(`features.subsections.${activeNavItem}.paragraph`)}
       </Paragraph>
       <Button styleName="white">{t("features.registerForFree")}</Button>
     </Box>
-    <Box width={[1, 1 / 2]} p={2}>
-      {/* TODO: optimize visibility.png */}
+    <Box
+      width={[1]}
+      m={2}
+      css={{
+        maxWidth: "512px"
+      }}
+    >
       <Screen activeNavItem={activeNavItem} />
     </Box>
   </Flex>
--- sections/landing/Features/Template.styled.js
+++ sections/landing/Features/Template.styled.js
@@ -4,4 +4,5 @@ export const Screen = styled.img.attrs(p => ({
   src: `/static/img/features/${p.activeNavItem}.png`
 }))`
   width: 100%;
+  max-width: 512px;
 `;
