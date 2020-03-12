42ebe42d61d336de3d19d1465054678f83440331
Lemcur <rafal.klimek@netguru.co>
2020-03-12 17:12:19 +0100

    .

--- deployment/config/deploy/staging.rb
+++
@@ -1,9 +0,0 @@
-server 'sherlock.staging.devguru.co', user: 'deploy', roles: %w[app web]
-set :branch, 'master'
-set :dockerfile, -> { 'docker/Dockerfile.staging' }
-set :capose_commands, -> {
-  [
-    'build',
-    'up -d',
-  ]
-}
