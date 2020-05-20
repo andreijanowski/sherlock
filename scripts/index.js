4d1b9e7768a5da1dfbddb9edba0d24d8d7f1cbb4
Dariusz Przymuszala <dariusz.przymuszala@netguru.co>
2020-05-20 16:58:33 +0200

    add script

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 export { default as FacebookPixelScript } from "./facebookPixel";
+export { default as HubspotChatScript } from "./hubspotChat";
 export {
   default as GoogleTagManagerScript,
   noscript as GoogleTagManagerNoscript
