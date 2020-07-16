20cf8baa68b5ed32a364d0c532bc1b080b0557f1
M G Cencek <m.g.cencek@icloud.com>
2020-07-16 16:04:09 +0200

    [TFD-2337] update endpoint

--- data/actions/wholesalers.js
+++ data/actions/wholesalers.js
@@ -3,7 +3,7 @@ import { FETCH_WHOLESALERS_REQUEST } from "types/wholesalers";
 export const fetchWholesalers = (page = 1) => ({
   type: FETCH_WHOLESALERS_REQUEST,
   payload: {
-    endpoint: "/api/v1/partners",
+    endpoint: "/api/v1/partners/wholesalers",
     params: {
       page,
       per_page: 200
