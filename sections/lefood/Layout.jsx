95a72f2930d108cbb28be215b44828c62084229c
maciej dabek <maciej.dabek@netguru.com>
2021-02-08 11:05:15 +0100

    fix: delivery split fix

--- sections/lefood/Layout.jsx
+++ sections/lefood/Layout.jsx
@@ -50,7 +50,7 @@ const splitRatioList = [
   { value: "0.25", label: "25%" },
   { value: "0.5", label: "50%" },
   { value: "0.75", label: "75%" },
-  { value: "1", label: "100%" }
+  { value: "1.0", label: "100%" }
 ];
 
 const averageDeliveryTimeList = t => [
