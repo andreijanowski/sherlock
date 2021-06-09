2a3f16434c571b477a98a8f946795300524c9ee9
Grigory Solovyov <g.solovev@andersenlab.com>
2021-06-09 14:59:59 +0300

    Change endpoint buildind for detectives
    TFD-2799

--- data/actions/detectives.js
+++ data/actions/detectives.js
@@ -15,22 +15,21 @@ export const fetchTopDetective = () => ({
   meta: { thunk: true }
 });
 
-export const fetchDetectives = ({ city = "", excludeCities = [] }) => {
-  const endpoint = excludeCities.length
-    ? `/api/v1/users/detectives?${excludeCities
-        .map(excludeCity => `filter[exclude_cities][]=${excludeCity}`)
-        .join("&")}`
-    : `/api/v1/users/detectives?filter[city]=${city}`;
-
-  return {
-    type: FETCH_DETECTIVES_REQUEST,
-    payload: {
-      endpoint,
-      params: {
-        page: 1,
-        per_page: 5
-      }
-    },
-    meta: { thunk: true, city }
-  };
-};
+export const fetchDetectives = ({ city = "", excludeCities = [] }) => ({
+  type: FETCH_DETECTIVES_REQUEST,
+  payload: {
+    endpoint: "/api/v1/users/detectives",
+    params: {
+      page: 1,
+      per_page: 5,
+      ...(excludeCities.length
+        ? {
+            "filter[exclude_cities]": excludeCities
+          }
+        : {
+            "filter[city]": city
+          })
+    }
+  },
+  meta: { thunk: true, city }
+});
