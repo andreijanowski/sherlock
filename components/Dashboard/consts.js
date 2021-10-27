2baf19798de5d1a19a332324be63ef3f5f36daee
Grigory Solovyov <kefan8@mail.ru>
2021-10-27 13:17:10 +0300

    Add translations constants
    FOOD-127

--- components/Dashboard/consts.js
+++ components/Dashboard/consts.js
@@ -1,57 +1,65 @@
+const TRANSLATIONS = {
+  TODAY: "today",
+  YESTERDAY: "yesterday",
+  LAST_MONTH: "lastMonth",
+  LAST_3_MONTH: "last3Months",
+  LAST_YEAR: "lastYear"
+};
+
 export const OPTIONS = [
   {
-    name: "yesterday",
+    name: TRANSLATIONS.YESTERDAY,
     value: "yesterday"
   },
   {
-    name: "lastMonth",
+    name: TRANSLATIONS.LAST_MONTH,
     value: "lastMonth"
   },
   {
-    name: "last3Months",
+    name: TRANSLATIONS.LAST_3_MONTH,
     value: "last3Months"
   },
   {
-    name: "lastYear",
+    name: TRANSLATIONS.LAST_YEAR,
     value: "lastYear"
   }
 ];
 
 export const OPTIONS_FOR_REVENUE = [
-  { name: "today", value: "sumToday" },
+  { name: TRANSLATIONS.TODAY, value: "sumToday" },
   {
-    name: "yesterday",
+    name: TRANSLATIONS.YESTERDAY,
     value: "sumYesterday"
   },
   {
-    name: "lastMonth",
+    name: TRANSLATIONS.LAST_MONTH,
     value: "sumLastMonth"
   },
   {
-    name: "last3Months",
+    name: TRANSLATIONS.LAST_3_MONTH,
     value: "sumLast3Months"
   },
   {
-    name: "lastYear",
+    name: TRANSLATIONS.LAST_YEAR,
     value: "sumLastYear"
   }
 ];
 
 export const SALES_OPTIONS = [
   {
-    name: "yesterday",
+    name: TRANSLATIONS.YESTERDAY,
     value: "day"
   },
   {
-    name: "lastMonth",
+    name: TRANSLATIONS.LAST_MONTH,
     value: "month"
   },
   {
-    name: "last3Months",
+    name: TRANSLATIONS.LAST_3_MONTH,
     value: "quarter"
   },
   {
-    name: "lastYear",
+    name: TRANSLATIONS.LAST_YEAR,
     value: "year"
   }
 ];
