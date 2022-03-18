0164a9068e790d24d3381f6865bf384326045c4a
Piotr Tylicki <piotr.tylicki@netguru.co>
2022-03-18 12:18:42 +0100

    hotfix: expire date param

--- server/handleOauthCallback.js
+++ server/handleOauthCallback.js
@@ -7,7 +7,7 @@ function handleOauthCallback(req, res) {
   let registrationCallback = false;
   let { state } = req.query;
 
-  if (typeof state != 'undefined' && state.endsWith("registration")) {
+  if (typeof state !== "undefined" && state.endsWith("registration")) {
     state = state.substring(0, state.length - 12);
     registrationCallback = true;
   }
