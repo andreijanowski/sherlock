c091a2abdb004f7f2a9a3da2f5fb6a5cd8780fb7
Piotr Tylicki <piotr.tylicki@netguru.co>
2022-09-07 12:30:06 +0200

    update breakdown revenue with translations

--- components/Dashboard/consts.js
+++ components/Dashboard/consts.js
@@ -1,9 +1,15 @@
-const TRANSLATIONS = {
+export const TRANSLATIONS = {
   TODAY: "today",
   YESTERDAY: "yesterday",
   LAST_MONTH: "lastMonth",
   LAST_3_MONTH: "last3Months",
-  LAST_YEAR: "lastYear"
+  LAST_YEAR: "lastYear",
+  THIS_MONTH: "thisMonth",
+  THIS_YEAR: "thisYear",
+  THIS_QUARTER: "thisQuarter",
+  PREVIOUS_MONTH: "previousMonth",
+  PREVIOUS_QUARTER: "previousQuarter",
+  PREVIOUS_YEAR: "previousYear"
 };
 
 export const OPTIONS = [
@@ -22,5 +28,40 @@ export const OPTIONS = [
   {
     name: TRANSLATIONS.LAST_YEAR,
     value: "year"
+  }
+];
+
+export const RB_OPTIONS = [
+  {
+    name: TRANSLATIONS.TODAY,
+    value: TRANSLATIONS.TODAY
+  },
+  {
+    name: TRANSLATIONS.YESTERDAY,
+    value: TRANSLATIONS.YESTERDAY
+  },
+  {
+    name: TRANSLATIONS.THIS_MONTH,
+    value: TRANSLATIONS.THIS_MONTH
+  },
+  {
+    name: TRANSLATIONS.PREVIOUS_MONTH,
+    value: TRANSLATIONS.PREVIOUS_MONTH
+  },
+  {
+    name: TRANSLATIONS.THIS_QUARTER,
+    value: TRANSLATIONS.THIS_QUARTER
+  },
+  {
+    name: TRANSLATIONS.PREVIOUS_QUARTER,
+    value: TRANSLATIONS.PREVIOUS_QUARTER
+  },
+  {
+    name: TRANSLATIONS.THIS_YEAR,
+    value: TRANSLATIONS.THIS_YEAR
+  },
+  {
+    name: TRANSLATIONS.LAST_YEAR,
+    value: TRANSLATIONS.LAST_YEAR
   }
 ];
