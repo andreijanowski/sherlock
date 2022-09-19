89a0ca1f58cb49e3fcbace0c38a43972dd527de2
Piotr Tylicki <piotr.tylicki@netguru.co>
2022-09-19 12:18:23 +0200

    fix typeform component

--- components/TypeformContainer/index.jsx
+++ components/TypeformContainer/index.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import { string } from "prop-types";
 import { createWidget } from "@typeform/embed";
+import { Widget } from "@typeform/embed-react";
 import "@typeform/embed/build/css/widget.css";
 
 import { Container } from "./styled";
 
 const TypeformContainer = ({ formId }) => {
   const containerRef = useRef(null);
+  console.log("REF", containerRef, formId);
   useEffect(() => {
     if (containerRef.current) {
       try {
@@ -14,11 +16,19 @@ const TypeformContainer = ({ formId }) => {
           container: containerRef.current
         });
       } catch (err) {
-        console.error(err);
+        console.error("Error:", err);
       }
     }
   }, [formId]);
-  return <Container ref={containerRef} />;
+  return (
+    <Container ref={containerRef}>
+      <Widget
+        id={formId}
+        style={{ height: "500px", width: "100%" }}
+        className="my-form"
+      />
+    </Container>
+  );
 };
 
 TypeformContainer.propTypes = {
