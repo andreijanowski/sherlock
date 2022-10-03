7cd2fa885885b97831bd0ac30055604dbd5288bb
Aleksander <aleksander@foodetective.co>
2022-10-03 16:27:36 +0200

    fix: update triangle narrow position

--- components/LandingNavigation/NavigationList/NavigationListNestedLink/styled.js
+++ components/LandingNavigation/NavigationList/NavigationListNestedLink/styled.js
@@ -10,19 +10,6 @@ export const PopupContainer = styled.div`
   box-shadow: 0px 4px 27px rgba(${p => p.theme.colors.black}, 0.3);
   border-radius: 13px;
 
-  &:before {
-    content: ' ';
-    display: block;
-    border-left: 8px solid transparent;
-    border-right: 8px solid transparent;
-    border-bottom: 8px solid white;
-    width: 0px;
-    position: absolute;
-    top: -8px;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-
   ${downThanBreakpoint(2)} {
     display: block;
     padding: 16px;
@@ -35,6 +22,23 @@ export const PopupContainer = styled.div`
 export const NestedLinkContainer = styled(Flex)`
   color: rgb(${p => p.theme.colors.white});
   cursor: pointer;
+  position: relative;
+  
+  &[aria-expanded='true'] {
+    &:before {
+      content: ' ';
+      display: block;
+      border-left: 8px solid transparent;
+      border-right: 8px solid transparent;
+      border-bottom: 8px solid white;
+      width: 0;
+      position: absolute;
+      bottom: -11px;
+      left: 50%;
+      transform: translateX(-50%);
+      z-index: 999999;
+    }
+  }
 `;
 
 export const Link = styled.a`
