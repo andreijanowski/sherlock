7400b5fdd2e6e91703af6ee54a4147cfad4f25c7
Aleksander <aleksander@foodetective.co>
2022-11-04 11:00:07 +0100

    feat: suppliers debounce search & close icon

--- components/Algolia/ConnectedFilter.jsx
+++ components/Algolia/ConnectedFilter.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { connectSearchBox } from "react-instantsearch-dom";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { func, string } from "prop-types";
+import debounce from "debounce";
 import { CartIcon, ClockIcon, HeartIcon } from "../Icons";
+
+const DEBOUNCE = 300;
 
 const CustomFilter = React.forwardRef((props, myRef) => {
   const { currentRefinement, refine, label, placeholder } = props;
-  const [state, setState] = useState({ name: currentRefinement });
+  const [value, setValue] = useState(currentRefinement);
 
-  const handleChange = evt => {
-    const { target } = evt;
+  const onSubmit = useCallback(
+    query => {
+      refine(query);
+    },
+    [refine]
+  );
 
-    setState({
-      ...state,
-      [target.name]: target.type === "checkbox" ? target.checked : target.value
-    });
-  };
+  const debouncedOnChange = useCallback(debounce(onSubmit, DEBOUNCE), [
+    onSubmit
+  ]);
 
-  const handleSubmit = ({ name }) => {
-    refine(name);
-  };
+  const clearSearch = useCallback(() => {
+    setValue("");
+    debouncedOnChange("");
+  }, [debouncedOnChange]);
+
+  const handleChange = useCallback(
+    ({ target }) => {
+      setValue(target.value);
+      debouncedOnChange(target.value);
+    },
+    [debouncedOnChange]
+  );
 
   return (
     <div className="py-2 px-4 rounded-lg shadow-card flex justify-between bg-white items-center my-6">
@@ -28,19 +42,22 @@ const CustomFilter = React.forwardRef((props, myRef) => {
       <div className="relative">
         <input
           ref={myRef}
-          id="name"
-          name="name"
+          value={value}
           placeholder={placeholder}
           defaultValue={currentRefinement}
           onChange={handleChange}
-          size="small"
           className="py-2 px-4 border border-gray-300 h-12 rounded min-w-100 pr-10 focus:outline-none"
         />
-        <FontAwesomeIcon
-          icon={faSearch}
-          className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-          onClick={() => handleSubmit(state)}
-        />
+        <div className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 flex items-center space-x-3">
+          {value && (
+            <FontAwesomeIcon
+              icon={faTimes}
+              onClick={clearSearch}
+              className="cursor-pointer"
+            />
+          )}
+          <FontAwesomeIcon icon={faSearch} className="cursor-pointer" />
+        </div>
       </div>
 
       <div className="flex space-x-4 flex-shrink-0">
